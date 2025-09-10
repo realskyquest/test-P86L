e2165afa9c823a4c9ec057dd3168f4ba1528e052
realskyquest <135049479+realskyquest@users.noreply.github.com>
2025-09-11 01:29:58 +0500

    p86l: add MergeRectangles

---
+++ utils.go
@@ -0,0 +1,53 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * SPDX-FileCopyrightText: 2025 Project 86 Community
+ *
+ * Project-86-Launcher: A Launcher developed for Project-86 for managing game files.
+ * Copyright (C) 2025 Project 86 Community
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package p86l
+
+import "image"
+
+func MergeRectangles(rects ...image.Rectangle) image.Rectangle {
+	if len(rects) == 0 {
+		return image.Rectangle{}
+	}
+
+	mergedMin := rects[0].Min
+	mergedMax := rects[0].Max
+
+	for _, r := range rects[1:] {
+		if r.Min.X < mergedMin.X {
+			mergedMin.X = r.Min.X
+		}
+		if r.Min.Y < mergedMin.Y {
+			mergedMin.Y = r.Min.Y
+		}
+		if r.Max.X > mergedMax.X {
+			mergedMax.X = r.Max.X
+		}
+		if r.Max.Y > mergedMax.Y {
+			mergedMax.Y = r.Max.Y
+		}
+	}
+
+	return image.Rectangle{
+		Min: mergedMin,
+		Max: mergedMax,
+	}
+}
