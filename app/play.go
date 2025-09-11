f1c32e88ae5b002e06af9b7cde42cea5d960585a
realskyquest <135049479+realskyquest@users.noreply.github.com>
2025-09-11 06:42:57 +0500

    app/play: fin

--- app/play.go
+++ app/play.go
@@ -21,8 +21,135 @@
 
 package app
 
-import "github.com/hajimehoshi/guigui"
+import (
+	"image"
+	"p86l/assets"
+
+	"github.com/hajimehoshi/guigui"
+	"github.com/hajimehoshi/guigui/basicwidget"
+	"github.com/hajimehoshi/guigui/layout"
+)
 
 type Play struct {
 	guigui.DefaultWidget
+
+	installButton, updateButton, playButton                   basicwidget.Button
+	form                                                      basicwidget.Form
+	prereleaseText                                            basicwidget.Text
+	prereleaseToggle                                          basicwidget.Toggle
+	background                                                basicwidget.Background
+	changelogText                                             basicwidget.Text
+	websiteButton, githubButton, discordButton, patreonButton basicwidget.Button
+
+	mainLayout   layout.GridLayout
+	buttonLayout layout.GridLayout
+	socialLayout layout.GridLayout
+}
+
+func (p *Play) AppendChildWidgets(context *guigui.Context, appender *guigui.ChildWidgetAppender) {
+	appender.AppendChildWidget(&p.installButton)
+	appender.AppendChildWidget(&p.updateButton)
+	appender.AppendChildWidget(&p.playButton)
+	appender.AppendChildWidget(&p.form)
+	appender.AppendChildWidget(&p.background)
+	appender.AppendChildWidget(&p.changelogText)
+	appender.AppendChildWidget(&p.websiteButton)
+	appender.AppendChildWidget(&p.githubButton)
+	appender.AppendChildWidget(&p.discordButton)
+	appender.AppendChildWidget(&p.patreonButton)
+}
+
+func (p *Play) Build(context *guigui.Context) error {
+	p.installButton.SetText("Install")
+	p.updateButton.SetText("Update")
+	p.playButton.SetText("Play")
+
+	p.prereleaseText.SetValue("Enable Pre-release")
+	p.form.SetItems([]basicwidget.FormItem{
+		{
+			PrimaryWidget:   &p.prereleaseText,
+			SecondaryWidget: &p.prereleaseToggle,
+		},
+	})
+
+	p.changelogText.SetValue(`TEST
+
+	VER v15.15.15
+
+	GOOD`)
+	p.changelogText.SetAutoWrap(true)
+	p.changelogText.SetMultiline(true)
+
+	p.websiteButton.SetIcon(assets.IE)
+	p.githubButton.SetIcon(assets.Github)
+	p.discordButton.SetIcon(assets.Discord)
+	p.patreonButton.SetIcon(assets.Patreon)
+
+	u := basicwidget.UnitSize(context)
+	p.mainLayout = layout.GridLayout{
+		Bounds: context.Bounds(p).Inset(u / 2),
+		Heights: []layout.Size{
+			layout.FixedSize(u * 2),
+			layout.FixedSize(u * 2),
+			layout.FixedSize(int(float64(u) * 1.5)),
+			layout.FixedSize(p.changelogText.Measure(context, guigui.FixedWidthConstraints(context.Bounds(p).Dx()-u)).Y + u/2),
+			layout.FixedSize(int(float64(u) * 1.5)),
+		},
+		RowGap: u / 2,
+	}
+	p.buttonLayout = layout.GridLayout{
+		Bounds: p.mainLayout.CellBounds(0, 1),
+		Widths: []layout.Size{
+			layout.FlexibleSize(1),
+			layout.FixedSize(u * 4),
+			layout.FixedSize(u * 4),
+			layout.FixedSize(u * 4),
+			layout.FlexibleSize(1),
+		},
+		Heights: []layout.Size{
+			layout.FixedSize(u * 2),
+		},
+		ColumnGap: u / 2,
+	}
+	p.socialLayout = layout.GridLayout{
+		Bounds: p.mainLayout.CellBounds(0, 4),
+		Widths: []layout.Size{
+			layout.FlexibleSize(1),
+			layout.FixedSize(int(float64(u) * 1.5)),
+			layout.FixedSize(int(float64(u) * 1.5)),
+			layout.FixedSize(int(float64(u) * 1.5)),
+			layout.FixedSize(int(float64(u) * 1.5)),
+			layout.FlexibleSize(1),
+		},
+		ColumnGap: u / 2,
+	}
+
+	return nil
+}
+
+func (p *Play) Layout(context *guigui.Context, widget guigui.Widget) image.Rectangle {
+	switch widget {
+	case &p.installButton:
+		return p.buttonLayout.CellBounds(1, 0)
+	case &p.updateButton:
+		return p.buttonLayout.CellBounds(2, 0)
+	case &p.playButton:
+		return p.buttonLayout.CellBounds(3, 0)
+	case &p.form:
+		return p.mainLayout.CellBounds(0, 2)
+	case &p.background:
+		return p.mainLayout.CellBounds(0, 3)
+	case &p.changelogText:
+		return p.mainLayout.CellBounds(0, 3).Inset(basicwidget.UnitSize(context) / 4)
+	case &p.websiteButton:
+		return p.socialLayout.CellBounds(1, 0)
+	case &p.githubButton:
+		return p.socialLayout.CellBounds(2, 0)
+	case &p.discordButton:
+		return p.socialLayout.CellBounds(3, 0)
+	case &p.patreonButton:
+		return p.socialLayout.CellBounds(4, 0)
+	}
+
+	return image.Rectangle{}
 }
