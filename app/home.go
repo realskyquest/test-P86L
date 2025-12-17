6cccb1c3e9af8b0a25269e6fe2c64c901ae6fa20
realskyquest <135049479+realskyquest@users.noreply.github.com>
2025-12-17 23:10:38 +0500

    app/home: fix ui

--- app/home.go
+++ app/home.go
@@ -32,23 +32,37 @@ type Home struct {
 	guigui.DefaultWidget
 
 	background                                               basicwidget.Background
-	form                                                     basicwidget.Form
+	formPanel1, formPanel2                                   basicwidget.Panel
+	form1, form2                                             basicwidget.Form
 	welcomeText, installedText, playTimeText, lastPlayedText basicwidget.Text
 	usernameText, versionText, timeText, lastText            basicwidget.Text
 }
 
 func (h *Home) Build(context *guigui.Context, adder *guigui.ChildAdder) error {
 	adder.AddChild(&h.background)
-	adder.AddChild(&h.form)
+	adder.AddChild(&h.formPanel1)
+	adder.AddChild(&h.formPanel2)
+
+	model := context.Model(h, modelKeyModel).(*p86l.Model)
+	dataFile := model.Data().Get()
+
+	h.usernameText.SetAutoWrap(true)
+	h.installedText.SetAutoWrap(true)
+	h.timeText.SetAutoWrap(true)
+	h.lastText.SetAutoWrap(true)
 
 	h.welcomeText.SetValue(p86l.T("home.welcome"))
 	h.usernameText.SetValue(p86l.GetUsername())
 	h.installedText.SetValue(p86l.T("home.version"))
-	h.versionText.SetValue("v1.8.2-alpha")
+	if dataFile.UsePreRelease {
+		h.versionText.SetValue(dataFile.InstalledPreRelease)
+	} else {
+		h.versionText.SetValue(dataFile.InstalledGame)
+	}
 	h.playTimeText.SetValue(p86l.T("home.time"))
 	h.lastPlayedText.SetValue(p86l.T("home.last"))
 
-	h.form.SetItems([]basicwidget.FormItem{
+	h.form1.SetItems([]basicwidget.FormItem{
 		{
 			PrimaryWidget:   &h.welcomeText,
 			SecondaryWidget: &h.usernameText,
@@ -57,6 +71,9 @@ func (h *Home) Build(context *guigui.Context, adder *guigui.ChildAdder) error {
 			PrimaryWidget:   &h.installedText,
 			SecondaryWidget: &h.versionText,
 		},
+	})
+
+	h.form2.SetItems([]basicwidget.FormItem{
 		{
 			PrimaryWidget:   &h.playTimeText,
 			SecondaryWidget: &h.timeText,
@@ -66,6 +83,14 @@ func (h *Home) Build(context *guigui.Context, adder *guigui.ChildAdder) error {
 			SecondaryWidget: &h.lastText,
 		},
 	})
+
+	h.formPanel1.SetContent(&h.form1)
+	h.formPanel1.SetAutoBorder(true)
+	h.formPanel1.SetContentConstraints(basicwidget.PanelContentConstraintsFixedWidth)
+
+	h.formPanel2.SetContent(&h.form2)
+	h.formPanel2.SetAutoBorder(true)
+	h.formPanel2.SetContentConstraints(basicwidget.PanelContentConstraintsFixedWidth)
 
 	return nil
 }
@@ -80,19 +105,25 @@ func (h *Home) Layout(context *guigui.Context, widgetBounds *guigui.WidgetBounds
 			},
 			{
 				Widget: &h.background,
-				Size:   guigui.FixedSize(h.form.Measure(context, guigui.FixedWidthConstraints(widgetBounds.Bounds().Dx())).Y + u),
+				Size:   guigui.FixedSize(h.Measure(context, guigui.FixedWidthConstraints(widgetBounds.Bounds().Dx())).Y - int(float64(u)*2.5)),
 				Layout: guigui.LinearLayout{
-					Direction: guigui.LayoutDirectionVertical,
+					Direction: guigui.LayoutDirectionHorizontal,
 					Items: []guigui.LinearLayoutItem{
 						{
-							Widget: &h.form,
+							Widget: &h.formPanel1,
+							Size:   guigui.FlexibleSize(3),
+						},
+						{
+							Widget: &h.formPanel2,
+							Size:   guigui.FlexibleSize(2),
 						},
 					},
+					Gap: u / 2,
 					Padding: guigui.Padding{
-						Start:  u / 2,
-						Top:    u / 2,
-						End:    u / 2,
-						Bottom: u / 2,
+						Start:  u / 4,
+						Top:    u / 4,
+						End:    u / 4,
+						Bottom: u / 4,
 					},
 				},
 			},
